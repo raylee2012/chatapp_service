fc7d7d1d850361ec2b3424565d9030e472cda821
raylee2012 <76456967@qq.com>
2020-08-29 23:59:23 +0800

    springboot+mybatis服务搭建完成

---
+++ chatapp_main/src/main/java/com/example/chatapp_main/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.example.chatapp_main.mapper;
+
+import com.example.chatapp_main.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface UserMapper {
+
+    User findUserByUsernam(@Param("username") String username);
+
+    void insertSelective(User user);
+}
