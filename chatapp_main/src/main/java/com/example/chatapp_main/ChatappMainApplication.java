9f820b0fb40b1f63343159b1d70b4d5fec6cb407
raylee2012 <76456967@qq.com>
2020-08-30 14:36:04 +0800

    文件的上传与下载基类

--- chatapp_main/src/main/java/com/example/chatapp_main/ChatappMainApplication.java
+++ chatapp_main/src/main/java/com/example/chatapp_main/ChatappMainApplication.java
@@ -1,9 +1,14 @@
 package com.example.chatapp_main;
 
+import com.example.chatapp_main.pojo.FileProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		FileProperties.class
+})
 public class ChatappMainApplication {
 
 	public static void main(String[] args) {
