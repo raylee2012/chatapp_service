b1e37efac3cf771c419efcbd5a0d3bff10c69a32
raylee2012 <76456967@qq.com>
2020-09-04 17:42:27 +0800

    群组及消息表结构设计

--- chatapp_main/src/main/resources/db/chatapp.sql
+++ chatapp_main/src/main/resources/db/chatapp.sql
@@ -30,15 +30,45 @@ CREATE TABLE `contact` (
   PRIMARY KEY (`contact_id`)
 ) ENGINE=INNODB  DEFAULT CHARSET=utf8
 
-#新建群组表====设计中
+#新建群组表
 CREATE TABLE `group` (
   `group_id` VARCHAR(50) NOT NULL COMMENT '群编号',
   `group_name` VARCHAR(50) DEFAULT NULL COMMENT '群名称',
   `group_notice` VARCHAR(500) DEFAULT NULL COMMENT '群公告',
-  `group_note` VARCHAR(500) DEFAULT NULL COMMENT '群备注',
   `create_user_id` VARCHAR(50) NOT NULL COMMENT '创建人id',
   `create_time` TIMESTAMP NULL DEFAULT '0000-00-00 00:00:00' COMMENT '创建时间',
   `update_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`group_id`)
+) ENGINE=INNODB  DEFAULT CHARSET=utf8
+
+#新建群成员表
+CREATE TABLE `group_member` (
+  `group_member_id` VARCHAR(50) NOT NULL COMMENT '群成员编号',
+  `group_member_user_id` VARCHAR(50) DEFAULT NULL COMMENT '群成员用户编号',
+  `group_note` VARCHAR(500) DEFAULT NULL COMMENT '群备注',
+  `group_disturb` CHAR (1) not NULL DEFAULT '0' COMMENT '0未开启消息免打扰,1开启消息免打扰',
+  `group_to_contact` CHAR (1) not NULL DEFAULT '0' COMMENT '0不保存到通讯录,1保存到通讯录',
+  `group_top` CHAR (1)  not NULL DEFAULT '0' COMMENT '0消息未置顶,1消息置顶',
+  `group_nick_name` VARCHAR(50) DEFAULT NULL COMMENT '个人设置的群昵称',
+  `group_top_update_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '消息置顶状态更新时间',
+  `create_time` TIMESTAMP NULL DEFAULT '0000-00-00 00:00:00' COMMENT '创建时间',
+  `update_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`group_member_id`)
+) ENGINE=INNODB  DEFAULT CHARSET=utf8
+
+#消息表
+CREATE TABLE `message` (
+  `message_id` VARCHAR(50) NOT NULL COMMENT '消息编号',
+  `message_group_id` VARCHAR(50) NOT NULL COMMENT '消息所属群编号',
+  `message_create_user_id` VARCHAR(50) NOT NULL COMMENT '创建消息的用户编号',
+  `message_content` VARCHAR(500) DEFAULT NULL COMMENT '消息内容',
+  `message_attach_url` VARCHAR(1000) DEFAULT NULL COMMENT '附件消息文件地址',
+  `message_attach_name` VARCHAR(200) DEFAULT NULL COMMENT '附件消息文件名',
+  `message_attach_size` VARCHAR(200) DEFAULT NULL COMMENT '附件消息文件大小',
+  `message_revocation` CHAR (1) not NULL DEFAULT '0' COMMENT '0非撤回消息,1撤回消息',
+  `message_type` CHAR (1) not NULL DEFAULT '0' COMMENT '0文本消息,1图片消息,2语音消息,3视频消息,4位置消息',
+  `create_time` TIMESTAMP NULL DEFAULT '0000-00-00 00:00:00' COMMENT '创建时间',
+  `update_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`message_id`)
 ) ENGINE=INNODB  DEFAULT CHARSET=utf8
 
